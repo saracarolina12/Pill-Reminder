5ee586ff95f8c31c06e9e3bc3907728b9ff0f9ce
LuisR-jpg <laloro08@hotmail.com>
2023-10-27 02:13:16 -0600

    [conn] Working on nextAlarm endpoint

--- server/index.js
+++ server/index.js
@@ -289,7 +289,7 @@ app.get('/getPills', requireAuth, (req, res) => {
 app.get('/nextAlarm', requireAuth, (req, res) => {
     let curr = Date.parse(new Date()) - toTimestamp(6); // Mexico is in UTC-6
     req.session.nextAlarm.forEach((alarm) => {
-
+        if(alarm.next > curr) return res.status(200).json({ nextAlarm: alarm});
     });
 });
 
